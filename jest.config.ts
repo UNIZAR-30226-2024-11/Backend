4e8ef3d0db9aed2b62097fef75946756ad8dacb4
Dorian Wozniak <dorian.wozniak@gmail.com>
2024-03-22 16:29:18 +0100

    Intento implementación pruebas

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "jest"
+
+const config: Config = {
+  preset: "ts-jest",
+  rootDir: "./test",
+}
+
+export default config
--- test/controller/login.test.ts
+++ test/controller/login.test.ts
@@ -1,5 +1,35 @@
+import { loginController } from "controller"
+import { Request, Response } from "express"
+import { User } from "model"
+
 describe("loginController", () => {
-  it("Datos de login correctos", async () => {})
+  it("Datos de login correctos", async () => {
+    const req = {
+      body: {
+        email: "john.doe@unizar.es",
+        password: "my-secret-password",
+      },
+    } as Request
+
+    const res = {} as Response
+
+    const user = {
+      id: 1,
+      email: "john.doe@unizar.es",
+      username: "john.doe",
+      password: "my-secret-password",
+    } as User
+
+    // Mock findUserByEmail so that it alawys returns user
+    const findUserByEmail = jest.fn().mockResolvedValue(user)
+
+    await loginController(req, res)
+
+    expect(loginController).toHaveBeenCalled()
+    expect(findUserByEmail).toHaveBeenCalledWith("john.doe@unizar.es")
+    expect(res.status).toBe(200)
+    expect(res.json).toHaveBeenCalledWith({ token: expect.any(String) })
+  })
 
   it("Usuario no encontrado", async () => {})
 
