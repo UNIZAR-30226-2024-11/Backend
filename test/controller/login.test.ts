cd5f1e438b486b8bb679ac80e7f1f294432403cb
Dorian Wozniak <dorian.wozniak@gmail.com>
2024-03-22 13:19:57 +0100

    Añadido jest

---
+++ test/controller/login.test.ts
@@ -0,0 +1,7 @@
+describe("loginController", () => {
+  it("Datos de login correctos", async () => {})
+
+  it("Usuario no encontrado", async () => {})
+
+  it("Contraseña incorrecta", async () => {})
+})
---
+++ test/controller/register.test.ts
@@ -0,0 +1,5 @@
+describe("registerController", () => {
+  it("Datos de registro correctos", async () => {})
+  it("Nombre de usuario ya en uso", async () => {})
+  it("Correo ya en uso", async () => {})
+})
