96dac98ac398728ccbad4151e4f297f526a49492
JorgeHZG <841766@unizar.es>
2024-04-10 12:25:35 +0200

    Corregida sintaxis el fichero 'app.js' de cypress/integration

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -1,9 +1,9 @@
-/* global describe, it, cy */
+/* global cy */
 
 describe('Example test', () => {
-    it('Descripción de la prueba', () => {
-      // Acciones que quieres probar utilizando Cypress
-      cy.visit('https://localhost:8000')
-    })
+  it('Descripción de la prueba', () => {
+  // Acciones que quieres probar utilizando Cypress
+  cy.visit('https://localhost:8000')
   })
+})
   
