149880db8d095ee24bed3e19944d979e52695f78
JorgeHZG <841766@unizar.es>
2024-04-10 12:20:43 +0200

    Corregido formato del fichero 'app.js'

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8000;
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-  res.send('hello from simple server :)');
-});
+  res.send('hello from simple server :)')
+})
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at https://localhost:${PORT}`);
-});
+  console.log(`Example app listening at https://localhost:${PORT}`)
+})
