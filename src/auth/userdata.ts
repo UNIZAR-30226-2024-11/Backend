b4fc94b471748075c562fb4e411bd8a262039cba
Jorge <jorgejorgejorgehz@gmail.com>
2024-06-03 19:32:05 +0200

    modificada ruta "userdata" para pasar en la misma ruta el id del usuario

--- src/auth/userdata.ts
+++ src/auth/userdata.ts
@@ -4,7 +4,13 @@ import { findUserDataById, User } from "../model"; // Asegúrate de que la ruta 
 /**
  * Solicitud de datos de usuario
  */
-type UserDataRequest = Request<{}, {}, { id: number }>;
+//type UserDataRequest = Request<{}, {}, { id: number }>;
+
+interface UserDataRequest extends Request {
+  params: {
+    id: string; // Los parámetros de la URL son cadenas por defecto
+  };
+}
 
 type UserDataResponse = Response<{ error: string } | { email: string }>;
 
@@ -18,15 +24,15 @@ export const userDataController = async (
   req: UserDataRequest,
   res: UserDataResponse,
 ) => {
-  if (!req.body.id) {
-    res.status(400).json({ error: "Falta parámetro id" });
+  const userId = parseInt(req.params.id, 10); // Convierte el ID a un número
+  if (isNaN(userId)) {
+    res.status(400).json({ error: "ID de usuario no válido" });
     return;
   }
   try {
-    const { id } = req.body;
-    const user = await findUserDataById(id);
+    const user = await findUserDataById(userId);
     if (!user) {
-      res.status(404).json({ error: "Usuario no encontrado" });
+      res.status(401).json({ error: `Usuario con id ${userId} no encontrado` });
       return;
     }
 
